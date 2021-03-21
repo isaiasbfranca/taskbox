9ca886067a91832a25638cde5bac955bb27bd041
Isaias Batista <isaiasbatista96@gmail.com>
2021-03-21 00:12:00 -0300

    taskbox UI

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //👇 Location of our stories
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
